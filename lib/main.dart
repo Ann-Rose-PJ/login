55b212c0a15132e8f0d35f8a5bdae2f952f28837
Ann-Rose-PJ <annrosepalathingal@gmail.com>
2024-07-10 14:20:50 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp( const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+Widget build(BuildContext context){
+  return const MaterialApp(
+    home: LoginPage(),
+  );
+}
+}
+
+class LoginPage extends StatelessWidget{
+  const LoginPage({super.key});
+
+  //bool rememberMe=false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login Page'),),
+      body: Form(
+          child: Column(
+          children: [
+            const Text('Welcome back',style:  TextStyle(fontWeight: FontWeight.w300,fontSize: 40)),
+            const SizedBox(height: 10),
+            const Text('Login to your Account',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 70)),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Password')              
+            ),
+           /* Row(
+              mainAxisAlignment:MainAxisAlignment.spaceBetween,
+              children:[
+                Row(children:[ Checkbox(value:rememberMe, onChanged: (value)
+            {setState(() {
+              rememberMe=value!;
+            });
+            },),
+            const Text('Remember me'),
+            ],),
+            TextButton(onPressed: (){}, child: const Text('Forgot password',style: TextStyle(color: Colors.blue)
+            ),),],
+            ),*/
+            ElevatedButton
+            (style: ElevatedButton.styleFrom(foregroundColor: const Color.fromRGBO(249, 252, 252, 1),backgroundColor:Colors.green ),            
+             child:const Text('Login now'),
+             onPressed: (){},
+             ),         
+            ElevatedButton(
+              style:ElevatedButton.styleFrom(
+              backgroundColor: Colors.black,
+              foregroundColor: Colors.white,elevation: 5),
+            child:  const Text ('Continue with Google'),
+             onPressed: () {
+               
+             },
+             ),
+             ],
+            ),
+          
+        ),
+        
+    );
+  }
+}
